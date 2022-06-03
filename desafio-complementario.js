57b468f6e62f45d54d3831158b9fcac81cff1cef
Agustin Belizan <agustinbelizan@icloud.com>
2022-06-03 00:18:58 -0300

    resuelto

--- desafio-complementario.js
+++ desafio-complementario.js
@@ -1,15 +1,12 @@
-let cantAlumnos = Number(prompt("Cuantos alumnos hay"));
-let edadAlumnos;
-let suma = 0;
-let i;
-
-let consigna = prompt("Deseas calcular el promedio por edad de tus alumnos?")
+let consigna = prompt("Deseas calcular el promedio por edad de tus alumnos?");
 if (consigna == "si"){
+    let cantAlumnos = Number(prompt("Cuantos alumnos hay"));
+    let edadAlumnos;
+    let suma = 0;
     for(i=1;i<=cantAlumnos;i++){
         edadAlumnos = Number(prompt(i + ". Edad?"));
     
         suma = suma + edadAlumnos;
     }  
+    alert(`El promedio de edades de ${cantAlumnos} alumnos es: ${suma/cantAlumnos}`);
 }
-
-console.log(`El promedio de edades de ${cantAlumnos} alumnos es: ${suma/cantAlumnos}`);
