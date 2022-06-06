53265ed6cdc56215060a4730cbe7bb81946af63a
Agustin Belizan <agustinbelizan@icloud.com>
2022-06-06 06:06:21 -0300

    primer desafio entregable

--- desafio-complementario.js
+++ desafio-complementario.js
@@ -1,12 +1,14 @@
 let consigna = prompt("Deseas calcular el promedio por edad de tus alumnos?");
-if (consigna == "si"){
-    let cantAlumnos = Number(prompt("Cuantos alumnos hay"));
-    let edadAlumnos;
-    let suma = 0;
-    for(i=1;i<=cantAlumnos;i++){
-        edadAlumnos = Number(prompt(i + ". Edad?"));
-    
-        suma = suma + edadAlumnos;
-    }  
-    alert(`El promedio de edades de ${cantAlumnos} alumnos es: ${suma/cantAlumnos}`);
+if (consigna == "si") {
+  let cantAlumnos = Number(prompt("Cuantos alumnos hay"));
+  let edadAlumnos;
+  let suma = 0;
+  for (i = 1; i <= cantAlumnos; i++) {
+    edadAlumnos = Number(prompt(i + ". Edad?"));
+
+    suma = suma + edadAlumnos;
+  }
+  alert(
+    `El promedio de edades de ${cantAlumnos} alumnos es: ${suma / cantAlumnos}`
+  );
 }
