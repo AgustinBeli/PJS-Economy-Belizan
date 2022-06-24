e8d75d97e73e691ca4b007853c1470eefef966ef
Agustin Belizan <agustinbelizan@icloud.com>
2022-06-24 00:09:59 -0300

    Desafio entregable n°2

--- desafio-entregable-2.js
+++ desafio-entregable-2.js
@@ -13,11 +13,23 @@
 
 // TITULO
 
-let tituloUno = document.getElementById("numero");
-tituloUno.innerHTML = "N°2!";
+// let tituloUno = document.getElementById("numero");
+// tituloUno.innerHTML = "N°2!";
 
-let titulo = document.getElementById("desafio");
-titulo.innerHTML = "¡Desafio Entregable";
+// let titulo = document.getElementById("desafio");
+// titulo.innerHTML = "¡Desafio Entregable";
+
+const desafio = document.getElementById("desafio");
+const numero = document.getElementById("numero");
+const conjunto = document.getElementById("conjunto");
+const titulo = document.getElementById("titulo");
+
+conjunto.onsubmit = (e) => {
+    e.preventDefault();
+    titulo.innerHTML = `${desafio.value}${numero.value}`;
+    
+}
+
 
 // FORMULARIO
 
@@ -25,24 +37,9 @@ const nombre = document.getElementById("nombre");
 const apellido = document.getElementById("apellido");
 const edad = document.getElementById("edad");
 const formu = document.getElementById("formu");
-const dato1 = document.getElementById("dato1");
-const dato2 = document.getElementById("dato2");
-const dato3 = document.getElementById("dato2");
 const presentacion = document.getElementById("presentacion");
 
-nombre.addEventListener("input", (e) => {
-  dato1.innerHTML = e.target.value;
-});
-apellido.addEventListener("input", (e) => {
-  dato2.innerHTML = e.target.value;
-});
-edad.addEventListener("input", (e) => {
-  dato3.innerHTML = e.target.value;
-});
-
 formu.onsubmit = (e) => {
-    e.preventDefault ();
-    console.log(nombre.value)
-    console.log(apellido.value)
-    presentacion = `Bienvenido ${nombre.value} ${apellido.value}`;
-}
+  e.preventDefault();
+  presentacion.innerHTML = `Bienvenido ${nombre.value} ${apellido.value}`;
+};
