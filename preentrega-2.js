2ac675f138c53d75c85708045cbc7f1cb5244119
Agustin Belizan <agustinbelizan@icloud.com>
2022-06-30 23:07:00 -0300

    probando

--- preentrega-2.js
+++ preentrega-2.js
@@ -1,31 +1,49 @@
-// localStorage.setItem('bienvenida','Hola Agus');
+let nombreUsuario;
+let apellidoUsuario;
+let edadUsuario;
 
+const saludoEstudiantes = document.getElementById("saludoUsuario");
+const formulario = document.getElementById("formulario");
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const edad = document.getElementById("edad");
+const contFormulario = document.getElementById("contFormulario");
+const contenido = document.getElementById("contenido");
+// const formu = document.getElementById("formu");
 
-// const bienvenida = localStorage.getItem('bienvenida');
-
-
-// const saludoEstudiantes = document.getElementById("saludoUsuario");
-// saludoEstudiantes.innerHTML = bienvenida;
-
-
-
-
-
-
-
-
-
-//OBTENGO LOS DATOS
-const sueldo = document.getElementById("sueldo").value;
-const propina = document.getElementById("propina").value;
-const gasto = document.getElementById("gasto").value;
-const formu = document.getElementById("formu");
-const total = document.getElementById("total");
-//RESUELVO
-let ingresos = sueldo + propina - gasto;
-let resultado = ingresos - gasto;
-//MANDO AL HTML
-formu.onsubmit = (e) => {
-  e.preventDefault();
-  total.innerHTML = `resultado: ${resultado}`;
+const ocultar = () => {
+  contFormulario.style.display = "none";
+  contenido.innerHTML = `Hola ${nombreUsuario} ${apellidoUsuario}`;
 };
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  nombreUsuario = nombre.value;
+  apellidoUsuario = apellido.value;
+  edadUsuario = edad.value;
+
+  localStorage.setItem("nombreUsuario", nombre.value);
+  localStorage.setItem("apellidoUsuario", apellido.value);
+  localStorage.setItem("edadUsuario", edad.value);
+
+  ocultar();
+});
+
+if (!!nombreUsuario && !!apellidoUsuario && !!edadUsuario) {
+  ocultar();
+}
+
+// //OBTENGO LOS DATOS
+// const sueldo = document.getElementById("sueldo").value;
+// const propina = document.getElementById("propina").value;
+// const gasto = document.getElementById("gasto").value;
+// const formu = document.getElementById("formu");
+// const total = document.getElementById("total");
+// //RESUELVO
+// let ingresos = sueldo + propina - gasto;
+// let resultado = ingresos - gasto;
+// //MANDO AL HTML
+// formu.onsubmit = (e) => {
+//   e.preventDefault();
+//   total.innerHTML = `resultado: ${resultado}`;
+// };
