2424752e25c68719ddc4f5ae353dab7cf855200b
Agustin Belizan <agustinbelizan@icloud.com>
2022-08-04 21:16:02 -0300

    detalles

---
+++ backend/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const port = 3000;
+
+const transactions = [
+    {
+        "type": "Ingreso",
+        "spent": "fijo",
+        "metod": "efectivo",
+        "amount": "40000",
+        "category": "SUELDO",
+        "description": "Osaka",
+        "codigo": 0
+    }, {
+        "type": "Ingreso",
+        "spent": "fijo",
+        "metod": "tarjeta",
+        "amount": "40000",
+        "category": "SUELDO",
+        "description": "Osaka",
+        "codigo": 2
+    }, {
+        "type": "Egreso",
+        "metod": "mercadopago",
+        "amount": "4000",
+        "spent": "fijo",
+        "category": "SERVICIO",
+        "description": "Claro",
+        "codigo": 12
+    }];
+
+app.get("/", (req, res) => {
+    res.send(`Ok, ingresaron a localhost! ${port}`);
+});
+
+app.get("/transactions", (req, res) => {
+    res.send(transactions);
+});
+
+app.get("/transactions/:id", (req, res) => {
+    const codigo = req.params.id;
+    const selectedTransaction = transactions.filter(
+        (transaction) => transaction.codigo == codigo
+    );
+    res.send(selectedTransaction);
+});
+
+app.post("/transactions", (req, res) => {
+    const transaction = "Aca va la transaction que me vino";
+    transactions.push(transaction);
+    res.send("Todo ok");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,96 +1,153 @@
 const formulario = document.getElementById("formulario");
 
+var todasLasTransacciones;
+
 formulario.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let datosFormulario = new FormData(formulario);
-  let info = objeto(datosFormulario);
-  guardar(info);
-  agregarDatos(info);
+    e.preventDefault();
+    let datosFormulario = new FormData(formulario);
+    let info = objeto(datosFormulario);
+    if (validando(info)) {
+        guardar(info);
+        agregarDatos(info);
+        formulario.reset();
+    } else { }
 });
+
+function validando(info) {
+    let validar = true;
+    if (!info["type"]) {
+        alert("Por favor debe seleccionar un tipo de operación");
+        validar = false;
+    }
+    if (!info["metod"]) {
+        alert("Por favor selecciona un medio de pago");
+        validar = false;
+    }
+    if (!info["amount"]) {
+        alert("Por favor ingrese un importe");
+        validar = false;
+    } else if (info["amount"] < 0) {
+        alert("No puedo ingresar un importe menor a 0");
+        validar = false;
+    }
+    if (!info["spent"]) {
+        alert("Por favor selecciona un método de transacción");
+        validar = false;
+    }
+    if (!info["category"]) {
+        alert("Por favor seleccione una de las opciones");
+        validar = false;
+    }
+    if (!info["description"]) {
+        alert("Por favor ingrese una descripción");
+        validar = false;
+    }
+    return validar;
+}
+
+// document.addEventListener("DOMContentLoaded", function (e) {
+//     let recorrer = JSON.parse(localStorage.getItem("datos"));
+//     recorrer.forEach(function (recorriendo) {
+//         agregarDatos(recorriendo)
+//     }
+//     )
+// });
 
 document.addEventListener("DOMContentLoaded", function (e) {
-  let recorrer = JSON.parse(localStorage.getItem("datos"));
-  recorrer.forEach(function (recorriendo) {
-    agregarDatos(recorriendo)
-  }
-  )
+    draw_category();
+    fetch("http://localhost:3000/transactions")
+        .then((res) => res.json())
+        .then((data) => {
+            mostrarEnPantallaArrayDeTransaccion(data);
+            todasLasTransacciones = data;
+        });
 });
 
+function mostrarEnPantallaArrayDeTransaccion(recorrer) {
+    recorrer.forEach(function (arrayElement) {
+        agregarDatos(arrayElement);
+    });
+}
+
+function getTransactionsFromApi() {
+    const allTransactions = fetch("http://localhost:3000/transactions");
+    return allTransactions;
+}
+
 function agregarCodigo() {
-  let ultimoCodigo = localStorage.getItem("ultimoCodigo") || "-1";
-  let nuevoCodigo = JSON.parse(ultimoCodigo) + 1;
-  localStorage.setItem("ultimoCodigo", JSON.stringify(nuevoCodigo));
-  return nuevoCodigo;
+    let ultimoCodigo = localStorage.getItem("ultimoCodigo") || "-1";
+    let nuevoCodigo = JSON.parse(ultimoCodigo) + 1;
+    localStorage.setItem("ultimoCodigo", JSON.stringify(nuevoCodigo));
+    return nuevoCodigo;
 }
 
 function objeto(datosFormulario) {
-  let type = datosFormulario.get("type");
-  let metod = datosFormulario.get("metod");
-  let amount = datosFormulario.get("amount");
-  let spent = datosFormulario.get("spent");
-  let category = datosFormulario.get("category");
-  let description = datosFormulario.get("description");
-  let codigo = agregarCodigo();
-  return {
-    type: type,
-    metod: metod,
-    amount: amount,
-    spent: spent,
-    category: category,
-    description: description,
-    codigo: codigo,
-  };
+    let type = datosFormulario.get("type");
+    let metod = datosFormulario.get("metod");
+    let amount = datosFormulario.get("amount");
+    let spent = datosFormulario.get("spent");
+    let category = datosFormulario.get("category");
+    let description = datosFormulario.get("description");
+    let codigo = agregarCodigo();
+    return {
+        type: type,
+        metod: metod,
+        amount: amount,
+        spent: spent,
+        category: category,
+        description: description,
+        codigo: codigo,
+    };
 }
 
 function agregarDatos(info) {
-  let historial = document.getElementById("historial");
+    let historial = document.getElementById("historial");
 
-  let nuevaFila = historial.insertRow(-1);
-  nuevaFila.setAttribute("data-codigo", info["codigo"]);
+    let nuevaFila = historial.insertRow(-1);
+    nuevaFila.setAttribute("data-codigo", info["codigo"]);
 
-  let nuevaCelda = nuevaFila.insertCell(0);
-  nuevaCelda.textContent = info["type"];
+    let nuevaCelda = nuevaFila.insertCell(0);
+    nuevaCelda.textContent = info["type"];
 
-  nuevaCelda = nuevaFila.insertCell(1);
-  nuevaCelda.textContent = info["metod"];
+    nuevaCelda = nuevaFila.insertCell(1);
+    nuevaCelda.textContent = info["metod"];
 
-  nuevaCelda = nuevaFila.insertCell(2);
-  nuevaCelda.textContent = info["amount"];
+    nuevaCelda = nuevaFila.insertCell(2);
+    nuevaCelda.textContent = info["amount"];
 
-  nuevaCelda = nuevaFila.insertCell(3);
-  nuevaCelda.textContent = info["spent"];
+    nuevaCelda = nuevaFila.insertCell(3);
+    nuevaCelda.textContent = info["spent"];
 
-  nuevaCelda = nuevaFila.insertCell(4);
-  nuevaCelda.textContent = info["category"];
+    nuevaCelda = nuevaFila.insertCell(4);
+    nuevaCelda.textContent = info["category"];
 
-  nuevaCelda = nuevaFila.insertCell(5);
-  nuevaCelda.textContent = info["description"];
+    nuevaCelda = nuevaFila.insertCell(5);
+    nuevaCelda.textContent = info["description"];
 
-  let eliminarCelda = nuevaFila.insertCell(6);
-  let botonDeshacer = document.createElement("button");
-  botonDeshacer.textContent = "❌";
-  eliminarCelda.appendChild(botonDeshacer);
+    let eliminarCelda = nuevaFila.insertCell(6);
+    let botonDeshacer = document.createElement("button");
+    botonDeshacer.textContent = "❌";
+    eliminarCelda.appendChild(botonDeshacer);
 
-  botonDeshacer.addEventListener("click", (e) => {
-    let celda = e.target.parentNode.parentNode;
-    let codigo = celda.getAttribute("data-codigo");
-    celda.remove();
-    deshacer(codigo);
-  });
+    botonDeshacer.addEventListener("click", (e) => {
+        let celda = e.target.parentNode.parentNode;
+        let codigo = celda.getAttribute("data-codigo");
+        celda.remove();
+        deshacer(codigo);
+    });
 }
 
 function deshacer(codigo) {
-  let recorrer = JSON.parse(localStorage.getItem("datos"));
-  let recorrerLocal = recorrer.findIndex((element) => element.codigo == codigo
-  );
-  recorrer.splice(recorrerLocal, 1);
-  let operacionJSON = JSON.stringify(recorrer);
-  localStorage.setItem("datos", operacionJSON);
+    let recorrer = JSON.parse(localStorage.getItem("datos"));
+    let recorrerLocal = recorrer.findIndex((element) => element.codigo == codigo);
+    recorrer.splice(recorrerLocal, 1);
+    let operacionJSON = JSON.stringify(recorrer);
+    localStorage.setItem("datos", operacionJSON);
 }
 
 function guardar(info) {
-  let operaciones = JSON.parse(localStorage.getItem("datos")) || [];
-  operaciones.push(info);
-  let operacionJSON = JSON.stringify(operaciones);
-  localStorage.setItem("datos", operacionJSON);
+    let operaciones = JSON.parse(localStorage.getItem("datos")) || [];
+    operaciones.push(info);
+    let operacionJSON = JSON.stringify(operaciones);
+    localStorage.setItem("datos", operacionJSON);
 }
